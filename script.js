8b429b18d5fe55f485b85a0c126249c01608d282
NishatTasnimSamiha30 <samsan3013@gmail.com>
2024-04-18 11:37:44 -0700

    jg

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-   
+    
+    slides[slideIndex - 1].style.display = 'block';
+    
 }
 
 function goNext() {
@@ -25,5 +27,5 @@ function goBack() {
     showSlides();
 }
 
-// call slideshow
+// showing slideshow
 showSlides();
